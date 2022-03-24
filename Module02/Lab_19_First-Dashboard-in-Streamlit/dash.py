6a17a59c293b1505dd2bdd061bc5e4ce071f67dd
Eric Martinet <93914292+eric-martinet@users.noreply.github.com>
2022-03-24 14:13:09 +0100

    Update dash.py

--- Module02/Lab_19_First-Dashboard-in-Streamlit/dash.py
+++ Module02/Lab_19_First-Dashboard-in-Streamlit/dash.py
@@ -12,6 +12,9 @@ breast_cancer_df["target"] = [breast_cancer.target_names[val] for val in breast_
 malignant_df = breast_cancer_df[breast_cancer_df["target"] == "malignant"]
 benign_df = breast_cancer_df[breast_cancer_df["target"] == "benign"]
 
+measurements = breast_cancer_df.columns.to_list()
+measurements.remove('target')
+
 
 # Global layout
 st.set_page_config(layout="wide")
@@ -24,26 +27,23 @@ default_cols = ["mean smoothness","mean compactness"]
 # Scatter chart with dropdown
 st.sidebar.markdown('### Dropdown with Scatter chart')
 
-measurements = breast_cancer_df.columns.to_list()
+
 x_axis_scatter = st.sidebar.selectbox('X axis', measurements)
 y_axis_scatter = st.sidebar.selectbox('Y axis', measurements)
 
+fig_scatter = plt.figure(figsize=figsize)
+ax_scatter = fig_scatter.add_subplot(1,1,1)
+
 if x_axis_scatter and y_axis_scatter:
-    fig_scatter = plt.figure(figsize=figsize)
-    ax_scatter = fig_scatter.add_subplot(1,1,1)
     malignant_df.plot.scatter(x=x_axis_scatter, y=y_axis_scatter, s=120, c="tomato", alpha=0.6, ax=ax_scatter, label="Malignant")
     benign_df.plot.scatter(x=x_axis_scatter, y=y_axis_scatter, s=120, c="dodgerblue", alpha=0.6, ax=ax_scatter, label="Benign")
-    ax_scatter.set_title(x_axis_scatter.title() + ' vs. ' + y_axis_scatter.title() + ' per Tumor Type')
-    ax_scatter.set_xlabel(x_axis_scatter.title())
-    ax_scatter.set_ylabel(y_axis_scatter.title())
 else:
-    fig_scatter = plt.figure(figsize=figsize)
-    ax_scatter = fig_scatter.add_subplot(1,1,1)
     malignant_df.plot.scatter(x=default_cols[0], y=default_cols[1], s=120, c="tomato", alpha=0.6, ax=ax_scatter, label="Malignant")
     benign_df.plot.scatter(x=default_cols[0], y=default_cols[1], s=120, c="dodgerblue", alpha=0.6, ax=ax_scatter, label="Benign")
-    ax_scatter.set_title(x_axis_scatter.title() + ' vs. ' + y_axis_scatter.title() + ' per Tumor Type')
-    ax_scatter.set_xlabel(x_axis_scatter.title())
-    ax_scatter.set_ylabel(y_axis_scatter.title())
+
+ax_scatter.set_title(x_axis_scatter.title() + ' vs. ' + y_axis_scatter.title() + ' per Tumor Type')
+ax_scatter.set_xlabel(x_axis_scatter.title())
+ax_scatter.set_ylabel(y_axis_scatter.title())
 
 #st.write(fig_scatter)
 
@@ -55,18 +55,14 @@ l_select_bar = st.sidebar.multiselect('Measurements', measurements, key = 'bar',
 
 if l_select_bar:
     breast_cancer_df_subset_bar = breast_cancer_df.groupby('target').mean().loc[:,l_select_bar]
-    fig_bar = plt.figure(figsize=figsize)
-    ax_bar = fig_bar.add_subplot(1,1,1)
-    breast_cancer_df_subset_bar.plot.bar(ax=ax_bar)
-    ax_bar.set_title("Average Measurements per Tumor Type")
-    ax_bar.set_xlabel('Tumor Type')
 else:
     breast_cancer_df_subset_bar = breast_cancer_df.groupby('target').mean().loc[:,default_cols]
-    fig_bar = plt.figure(figsize=figsize)
-    ax_bar = fig_bar.add_subplot(1,1,1)
-    breast_cancer_df_subset_bar.plot.bar(ax=ax_bar)
-    ax_bar.set_title("Average Measurements per Tumor Type")
-    ax_bar.set_xlabel('Tumor Type')
+
+fig_bar = plt.figure(figsize=figsize)
+ax_bar = fig_bar.add_subplot(1,1,1)
+breast_cancer_df_subset_bar.plot.bar(ax=ax_bar, cmap = "Set1")
+ax_bar.set_title("Average Measurements per Tumor Type")
+ax_bar.set_xlabel('Tumor Type')
  
 #st.write(fig_bar)
 
@@ -79,9 +75,13 @@ nb_bin_hist = st.sidebar.radio('Number of bins',options = [5, 10, 50, 100])
 
 fig_hist = plt.figure(figsize=figsize)
 ax_hist = fig_hist.add_subplot(1,1,1)
-breast_cancer_df_subset_hist = breast_cancer_df.loc[:,l_select_hist]
 
-breast_cancer_df_subset_hist.plot.hist(bins=nb_bin_hist, alpha = 0.75, ax=ax_hist)
+if l_select_hist and nb_bin_hist:
+    breast_cancer_df_subset_hist = breast_cancer_df.loc[:,l_select_hist]
+else:
+    breast_cancer_df_subset_hist = breast_cancer_df.loc[:,default_cols]
+
+breast_cancer_df_subset_hist.plot.hist(bins=nb_bin_hist, alpha = 0.75, ax=ax_hist, cmap = "Set1")
 ax_hist.set_title("Distribution of Measurements")
 
 #st.write(fig_hist)
@@ -97,7 +97,7 @@ y_axis_hexbin = st.sidebar.selectbox("Y axis", measurements, index = 1, key = 'h
 fig_hexbin = plt.figure(figsize=figsize)
 ax_hexbin = fig_hexbin.add_subplot(111)
 
-breast_cancer_df.plot.hexbin(x=x_axis_hexbin, y=y_axis_hexbin, reduce_C_function=np.mean, gridsize=25, ax = ax_hexbin)
+breast_cancer_df.plot.hexbin(x=x_axis_hexbin, y=y_axis_hexbin, reduce_C_function=np.mean, gridsize=25, ax = ax_hexbin) 
 
 ax_hexbin.set_title("Concentration of Measurements")
 ax_hexbin.set_xlabel(x_axis_hexbin.title())
